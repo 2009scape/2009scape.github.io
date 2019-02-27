837ed253eb691033801c6508b4a05c60f74525a2
jarryd229 <jarrydbrighton@hotmail.com>
2019-02-27 12:00:11 +1100

    Update config.php

--- runescape/includes/config.php
+++ runescape/includes/config.php
@@ -2,6 +2,6 @@
 $config['database.host'] = "localhost";
 $config['database.user'] = "root";
 $config['database.pass'] = "";
-$config['database.name'] = "runescape2";
-$config['site.address'] = "http://localhost/runescape2";
+$config['database.name'] = "runescape";
+$config['site.address'] = "http://localhost/runescape";
 ?>
