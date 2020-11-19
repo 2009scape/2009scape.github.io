5c80a0785aebf3c0fe042ad9bb4d9f950968bdbe
dginovker <dcress01@uoguelph.ca>
2020-11-19 15:09:26 -0500

    Updated readme - Deleted old files

--- runescape/includes/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$config['database.host'] = "localhost";
-$config['database.user'] = "root";
-$config['database.pass'] = "";
-$config['database.name'] = "runescape";
-$config['site.address'] = "http://localhost:4000"; // Was http://localhost/runescape
-?>
