3e96d16fd51b98fd2910b88baa6a873cdf10db6e
dginovker <dcress01@uoguelph.ca>
2020-11-19 22:36:14 -0500

    Remove bottom search on all kbase articles

--- website-HTML/www.runescape.com/kbase/fun.js
+++ website-HTML/www.runescape.com/kbase/fun.js
@@ -1,14 +1,10 @@
 let fs = require('fs');
 files = fs.readdirSync(__dirname + '/guid/');
 
-let replaceMenubox = `Feel free to join our Discord for further help:
-<ul class="further">
-    <li><a href="https://discord.gg/43YPGND">Discord</a></li>
-</ul>
-`
+let replaceMenubox = ``
 
 files.forEach(function (file) {
     var contents = fs.readFileSync(__dirname + '/guid/' + file, 'utf8');
-    const regex = /                                    <div class="brown_box">[\s\S]*?                                    <\/div>/i;
+    const regex = /<div class="brown_box"><div class="inner_brown_box">[\s\S]*?<\/div><\/div>/i;
     fs.writeFileSync(__dirname + "/guid/" + file, contents.replace(regex, replaceMenubox));
 })
