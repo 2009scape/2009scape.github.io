5480cbea1499db72554cf1ef8f710bbb753614a6
dginovker <dcress01@uoguelph.ca>
2020-11-19 23:25:49 -0500

    That was way harder than it should have been - but KBASE IS EVEN BETTER

--- website-HTML/www.runescape.com/kbase/fun.js
+++ website-HTML/www.runescape.com/kbase/fun.js
@@ -1,10 +1,10 @@
 let fs = require('fs');
 files = fs.readdirSync(__dirname + '/guid/');
 
-let replaceMenubox = ``
+let replaceMenubox = `<p style="text-align: center;font-size:11px;font-weight:bold; margin-bottom: 0px;">LOLBACKTODO</p>`
 
 files.forEach(function (file) {
     var contents = fs.readFileSync(__dirname + '/guid/' + file, 'utf8');
-    const regex = /<div id="lang">[\s\S]*?<\/div>/i;
-    fs.writeFileSync(__dirname + "/guid/" + file, contents.replace(regex, replaceMenubox));
+    const regex = /<form method="get" action="[\s\S]*?<\/form>/i;
+    fs.writeFileSync(__dirname + "/guid/" + file, contents.replace(`</p><img src="https://runescape.wiki/images/f/f4/Red_partyhat.png?7ed30">`, `<img src="https://runescape.wiki/images/f/f4/Red_partyhat.png?7ed30"></p>`));
 })
