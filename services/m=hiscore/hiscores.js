7d7f7240d4a721c997a774de5878098649d7bc4b
dginovker <dcress01@uoguelph.ca>
2020-12-09 08:35:42 -0500

    Some more work

--- services/m=hiscore/hiscores.js
+++ services/m=hiscore/hiscores.js
@@ -1,15 +1,36 @@
-for (let i = 1; i <= 22; i++) {
-    row = document.getElementsByClassName(`row row${i}`)[0];
+let page = 0;
 
-    row.childNodes[3].replaceWith(document.createElement("td"));
-    row.childNodes[3].className = "alL";
-    row.childNodes[3].innerHTML = "PlayerName";
+function populateHSTable() {
+    for (let i = 1; i <= 22; i++) {
+        row = document.getElementsByClassName(`row row${i}`)[0];
 
-    row.childNodes[5].replaceWith(document.createElement("td"));
-    row.childNodes[5].className = "alL";
-    row.childNodes[5].innerHTML = "Level";
+        row.childNodes[3].replaceWith(document.createElement("td"));
+        row.childNodes[3].className = "alL";
+        row.childNodes[3].innerHTML = "PlayerName";
 
-    row.childNodes[7].replaceWith(document.createElement("td"));
-    row.childNodes[7].className = "alL";
-    row.childNodes[7].innerHTML = "XP,XP";
-} 
+        row.childNodes[5].replaceWith(document.createElement("td"));
+        row.childNodes[5].className = "alL";
+        row.childNodes[5].innerHTML = "Level";
+
+        row.childNodes[7].replaceWith(document.createElement("td"));
+        row.childNodes[7].className = "alL";
+        row.childNodes[7].innerHTML = "XP,XP";
+    }
+}
+
+
+// Handle page arrows
+document.getElementById("button-up").addEventListener("click", function (e) {
+    e.preventDefault();
+    if (page > 0) {
+        page--;
+    }
+    populateHSTable();
+});
+document.getElementById("button-down").addEventListener("click", function (e) {
+    e.preventDefault();
+    page++;
+    populateHSTable();
+});
+
+populateHSTable();
