7d2420ad287466839938592c2c63150423989f3c
dginovker <dcress01@uoguelph.ca>
2020-12-09 09:20:58 -0500

    some basic HS goin on

--- services/m=hiscore/hiscores.js
+++ services/m=hiscore/hiscores.js
@@ -1,20 +1,75 @@
 let page = 0;
 
-function populateHSTable() {
+const apiURL = "http://localhost:3000";
+
+let tableData = [];
+
+fetch(`${apiURL}/highscores/getPlayersByTotal`)
+    .then(response => response.json())
+    .then(result => {
+        console.log(result[0]);
+        tableData = result;
+        populateDefaultHSTable();
+    })
+    .catch(error => console.log('error', error));
+
+function populateDefaultHSTable() {
     for (let i = 1; i <= 22; i++) {
         row = document.getElementsByClassName(`row row${i}`)[0];
+        const playerData = tableData[i + 22 * page - 1];
+
+        row.childNodes[1].replaceWith(document.createElement("td"));
+        row.childNodes[1].className = "rankCol";
+        row.childNodes[1].innerHTML = i + 22 * page;
 
         row.childNodes[3].replaceWith(document.createElement("td"));
         row.childNodes[3].className = "alL";
-        row.childNodes[3].innerHTML = "PlayerName";
+        row.childNodes[3].innerHTML = `<a href="./hiscores.html">${playerData ? playerData.username : ""}</a>`;
+        row.childNodes[3].addEventListener("click", function (e) {
+            e.preventDefault();
+            loadUserTable(playerData.username);
+        });
 
         row.childNodes[5].replaceWith(document.createElement("td"));
         row.childNodes[5].className = "alL";
-        row.childNodes[5].innerHTML = "Level";
+        row.childNodes[5].innerHTML = playerData ? playerData.level : "";
 
         row.childNodes[7].replaceWith(document.createElement("td"));
         row.childNodes[7].className = "alL";
-        row.childNodes[7].innerHTML = "XP,XP";
+        row.childNodes[7].innerHTML = playerData ? Math.floor(playerData.xp).toLocaleString() : "";
+    }
+}
+
+function loadUserTable(username) {
+    fetch(`${apiURL}/highscores/playerSkills/${username}`)
+        .then(response => response.json())
+        .then(result => {
+            console.log(result[0]);
+            tableData = result;
+            populatePlayerHSTable();
+        })
+        .catch(error => console.log('error', error));
+}
+
+function populatePlayerHSTable() {
+    for (let i = 1; i <= 22; i++) {
+        row = document.getElementsByClassName(`row row${i}`)[0];
+
+        row.childNodes[1].replaceWith(document.createElement("td"));
+        row.childNodes[1].className = "rankCol";
+        row.childNodes[1].innerHTML = 0; //`sName[${i + 22 * page}]`;
+
+        row.childNodes[3].replaceWith(document.createElement("td"));
+        row.childNodes[3].className = "alL";
+        row.childNodes[3].innerHTML = `rankCompared`;
+
+        row.childNodes[5].replaceWith(document.createElement("td"));
+        row.childNodes[5].className = "alL";
+        row.childNodes[5].innerHTML = tableData[i].static;
+
+        row.childNodes[7].replaceWith(document.createElement("td"));
+        row.childNodes[7].className = "alL";
+        row.childNodes[7].innerHTML = Math.floor(tableData[i].experience).toLocaleString();
     }
 }
 
@@ -25,12 +80,12 @@ document.getElementById("button-up").addEventListener("click", function (e) {
     if (page > 0) {
         page--;
     }
-    populateHSTable();
+    populateDefaultHSTable();
 });
 document.getElementById("button-down").addEventListener("click", function (e) {
     e.preventDefault();
     page++;
-    populateHSTable();
+    populateDefaultHSTable();
 });
 
-populateHSTable();
+populateDefaultHSTable();
