03d24aa11e86bf2785a54f46e37f401a4e85c338
dginovker <danielginovker@gmail.com>
2022-11-13 21:06:18 +0900

    Checkpoint

--- services/m=hiscore/helpers.js
+++ services/m=hiscore/helpers.js
@@ -209,13 +209,13 @@ hiscores.getIronIcon = (ironStatus) => {
     switch (ironStatus) {
         case "1":
         case 1:
-            return `<img src="../../site/img/osrsimg/ironman.png" style="height: 11px"> `;
+            return `<img src="../../site/img/osrsimg/ironman.webp" style="height: 11px"> `;
         case "2":
         case 2:
-            return `<img src="../../site/img/osrsimg/hcim.png" style="height: 11px"> `;
+            return `<img src="../../site/img/osrsimg/hcim.webp" style="height: 11px"> `;
         case "3":
         case 3:
-            return `<img src="../../site/img/osrsimg/ultimateironman.png" style="height: 11px"> `;
+            return `<img src="../../site/img/osrsimg/ultimateironman.webp" style="height: 11px"> `;
         default:
             return "";
     }
